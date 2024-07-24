9252113bbed383c3a27e9d1660ed45eda7e13674
SemiHyeon <gustpal08@gmail.com>
2024-07-24 09:47:22 +0900

    5일차

---
+++ 현세미/DP/정수 삼각형.py
@@ -0,0 +1,13 @@
+def solution(triangle):
+
+    height = len(triangle)
+    
+
+    for i in range(height - 2, -1, -1):
+
+        for j in range(len(triangle[i])):
+           
+            triangle[i][j] = triangle[i][j] + max(triangle[i + 1][j], triangle[i + 1][j + 1])
+    
+  
+    return triangle[0][0]
