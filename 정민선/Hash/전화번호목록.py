8bbc866f1a4610b7e31cf34d7e81a1a0c5cc5e82
minsun <elinnane0@gmail.com>
2024-07-28 23:22:46 +0900

    [정민선] 전화번호 목록 수정

--- 정민선/Hash/전화번호목록.py
+++ 정민선/Hash/전화번호목록.py
@@ -1,8 +1,8 @@
 def solution(phone_book):
+    phone_book.sort()
     answer = True
-    last = phone_book[-1]
     
-    while(answer == True and last != phone_book[0]):
+    while(answer == True and len(phone_book) > 0):
         phone = phone_book.pop(0)
         n = len(phone)
         for j in phone_book:
@@ -10,10 +10,9 @@ def solution(phone_book):
                 answer = False
                 break
                 
-        phone_book.append(phone)
     return answer
 
 ## 효율성 3,4번 시간 초과가 난다..>!!
 
-phone_book =["123","456","789"]
+phone_book =["456","789","123"]
 print(solution(phone_book))
